426fc0310cfd93825e8e68512c88c1825714f671
mgondan <Matthias.Gondan@psy.ku.dk>
2021-05-21 19:12:09 +0200

    Update hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,47 +1,14 @@
 #include <SWI-CPP.h>
-#include <string.h>
+#include <iostream.h>
 
-static functor_t FUNCTOR_equal2;
-#define MAXNAME 512
-
-static foreign_t pl_hello(PlTerm In, PlTerm Out)
-{
-  return Out = PlCompound("hello", In) ; ;
-}
-
-static foreign_t pl_environ(term_t list)
+PREDICATE(hello, 1)
 { 
-  extern char **environ;
-  term_t tail = PL_copy_term_ref(list);
-  term_t head = PL_new_term_ref();
-  char **ep;
-
-  for(ep=environ; *ep; ep++)
-  { char *e = *ep;
-    char *en;
-
-    if ( (en=strchr(e, '=')) && en-e < MAXNAME )
-    { char name[MAXNAME];
-
-      strncpy(name, e, en-e);
-      name[en-e] = 0;
-
-      if ( !PL_unify_list(tail, head, tail) ||
-	   !PL_unify_term(head, PL_FUNCTOR, FUNCTOR_equal2,
-			    PL_MBCHARS, name,
-			    PL_MBCHARS, en+1) )
-	return FALSE;
-    }
-  }
-
-  return PL_unify_nil(tail);
+  cout << "Hello " << (char *)A1 << endl;
+  return TRUE;
 }
 
 install_t install_rologpp()
 { 
-  FUNCTOR_equal2 = PL_new_functor(PL_new_atom("="), 2);
-  PL_register_foreign("environ", 1, pl_environ, 0);
-  // PL_register_foreign("hello", 2, pl_hello, 0);
   PlRegister x_hello_1(NULL, "hello", 2, pl_hello);
 }
 
